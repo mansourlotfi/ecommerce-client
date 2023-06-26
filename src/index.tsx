84616e2a332a8143168172836a985924ddb75d2b
mansourlotfi <mansourlotfi@gmail.com>
2023-06-26 15:13:00 +0330

    Add: service worker

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ import { Provider } from "react-redux";
 import { store } from "./app/store/configureStore";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -24,6 +25,11 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
