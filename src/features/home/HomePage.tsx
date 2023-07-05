e82a8163073ab97acdab8bcb4ce0d6073536059d
mansourlotfi <mansourlotfi@gmail.com>
2023-07-05 13:38:15 +0330

    change home page ordering

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -27,10 +27,10 @@ export default function HomePage() {
     <>
       <TopSwiper />
       <Container>
+        <NewProducts />
         <Categories />
         <Brands />
         <FeturedProducts />
-        <NewProducts />
       </Container>
     </>
   );
